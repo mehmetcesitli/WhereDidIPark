274b3c5f45ef473d3bf6ed7d02642f3c434fb21d
Mehmet <mecesitli@gmailc>
2020-04-13 18:17:27 +0300

    Redundant variables has been deleted. LocationInfo class and DistanceBetween method added. Where am I button now displays the true distance to the parking spot from current location. A listener to Get me to my Car button added to redirect the user to google maps.

--- app/src/main/java/com/example/wheredidipark/MainActivity.java
+++ app/src/main/java/com/example/wheredidipark/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -16,6 +17,7 @@ import android.content.pm.PackageManager;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 //import android.location.LocationListener;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -38,30 +40,84 @@ import com.google.android.gms.location.LocationListener;
 public class MainActivity extends AppCompatActivity implements ConnectionCallbacks,OnConnectionFailedListener /*implements LocationListener*/ {
 
 
-    Button parkHereButton;
-    Button whereAmIButton;
-    Button getMeToMyCarButton;
-    TextView currentlyParked;
-    TextView parkingSpotAddress;
-    TextView parkingSpotCoordinates;
-    TextView currentLocationText;
-    TextView currentAddressText;
-    TextView currentLocationCoordinates;
-    TextView distanceToCarText;
+    private Button parkHereButton;
+    private Button whereAmIButton;
+    private Button getMeToMyCarButton;
+    private TextView currentlyParked;
+    private TextView parkingSpotAddress;
+    private TextView parkingSpotCoordinates;
+    private TextView currentLocationText;
+    private TextView currentAddressText;
+    private TextView currentLocationCoordinates;
+    private TextView distanceToCarText;
 
 
     private GoogleApiClient googleApiClient;
     private FusedLocationProviderClient fusedLocationProviderClient;
 
-    String latitude;
-    String longitude;
-    double dLatitude;
-    double dLongitude;
+    private double latitude;
+    private double longitude;
+    private String fullAddress;
 
     public static final int RequestPermissionCode = 1;
 
-    Geocoder geocoder;
-    List<Address> addresses;
+    private Geocoder geocoder;
+    private List<Address> addresses;
+
+    private LocationInfo parkingSpotLocation;
+    private LocationInfo currentLocation;
+
+    class LocationInfo{
+        private double latitude;
+        private double longitude;
+        private String address;
+
+        public double getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(double latitude) {
+            this.latitude = latitude;
+        }
+
+        public double getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(double longitude) {
+            this.longitude = longitude;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public LocationInfo (double latitude, double longitude, String address){
+            setLatitude(latitude);
+            setLongitude(longitude);
+            setAddress(address);
+        }
+
+    }
+
+    public String distanceBetween (LocationInfo l1, LocationInfo l2){
+
+        Location location1 = new Location("");
+        Location location2 = new Location("");
+
+        location1.setLatitude(l1.getLatitude());
+        location1.setLongitude(l1.getLongitude());
+        location2.setLatitude(l2.getLatitude());
+        location2.setLongitude(l2.getLongitude());
+
+        String distanceInKiloMeters = String.format("%.2f",location1.distanceTo(location2)/1000);
+
+        return distanceInKiloMeters;
+    }
 
 
     @Override
@@ -71,12 +127,12 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
         setContentView(R.layout.activity_main);
 
         parkHereButton = findViewById(R.id.parkHereButton);
+        whereAmIButton = findViewById(R.id.whereAmIButton);
+        getMeToMyCarButton = findViewById(R.id.getMeToMyCarButton);
+
         currentlyParked = findViewById(R.id.currentlyParkedText);
         parkingSpotAddress = findViewById(R.id.parkingSpotAddress);
         parkingSpotCoordinates = findViewById(R.id.parkingSpotCoordinates);
-
-        whereAmIButton = findViewById(R.id.whereAmIButton);
-        getMeToMyCarButton = findViewById(R.id.getMeToMyCarButton);
         currentLocationText = findViewById(R.id.currentLocationText);
         currentAddressText = findViewById(R.id.currentAddressText);
         currentLocationCoordinates = findViewById(R.id.currentLocationCoordinates);
@@ -86,15 +142,63 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
         parkingSpotCoordinates.setVisibility(View.INVISIBLE);
         parkingSpotAddress.setVisibility(View.INVISIBLE);
 
+        whereAmIButton.setVisibility(View.INVISIBLE);
+        getMeToMyCarButton.setVisibility(View.INVISIBLE);
+        currentLocationText.setVisibility(View.INVISIBLE);
+        currentAddressText.setVisibility(View.INVISIBLE);
+        currentLocationCoordinates.setVisibility(View.INVISIBLE);
+        distanceToCarText.setVisibility(View.INVISIBLE);
+
+
         parkHereButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+
+                parkingSpotLocation = new LocationInfo(latitude,longitude,fullAddress);
+
+                parkingSpotCoordinates.setText(parkingSpotLocation.getLatitude() + ", " + parkingSpotLocation.getLongitude());
+                parkingSpotAddress.setText(parkingSpotLocation.getAddress());
 
                 currentlyParked.setVisibility(View.VISIBLE);
                 parkingSpotCoordinates.setVisibility(View.VISIBLE);
                 parkingSpotAddress.setVisibility(View.VISIBLE);
+                whereAmIButton.setVisibility(View.VISIBLE);
 
             }
 
+        });
+
+
+        whereAmIButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+
+                currentLocation = new LocationInfo(latitude,longitude,fullAddress);
+
+                currentLocationCoordinates.setText(currentLocation.getLatitude() + ", " + currentLocation.getLongitude());
+                currentAddressText.setText(currentLocation.getAddress());
+                distanceToCarText.setText(distanceBetween(parkingSpotLocation,currentLocation)+ " km");
+
+                getMeToMyCarButton.setVisibility(View.VISIBLE);
+                currentLocationText.setVisibility(View.VISIBLE);
+                currentAddressText.setVisibility(View.VISIBLE);
+                currentLocationCoordinates.setVisibility(View.VISIBLE);
+                distanceToCarText.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        getMeToMyCarButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //Uri uri = Uri.parse("geo:37.7749,-122.4194");
+                Uri uri = Uri.parse("http://maps.google.com/maps?saddr="+currentLocation.getLatitude()+","+currentLocation.getLongitude()+"&daddr="+parkingSpotLocation.getLatitude()+","+parkingSpotLocation.getLongitude());
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+            }
         });
 
         googleApiClient = new GoogleApiClient.Builder(this)
@@ -147,13 +251,12 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
                         public void onSuccess(Location location) {
                             if (location != null) {
 
-                                dLatitude = location.getLatitude();
-                                dLongitude = location.getLongitude();
-                                latitude = String.valueOf(location.getLatitude());
-                                longitude = String.valueOf(location.getLongitude());
+                                latitude = location.getLatitude();
+                                longitude =location.getLongitude();
 
                                 try {
-                                    addresses = geocoder.getFromLocation(dLatitude,dLongitude,1);
+
+                                    addresses = geocoder.getFromLocation(latitude,longitude,1);
 
                                     String address = addresses.get(0).getAddressLine(0);
                                     String area = addresses.get(0).getLocality();
@@ -161,14 +264,12 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
                                     String country = addresses.get(0).getCountryName();
                                     String postalCode = addresses.get(0).getPostalCode();
 
-                                    String fullAddress = address+", "+ area +", "+ city + ", "+ country +", "+ postalCode;
-
-                                    parkingSpotAddress.setText(fullAddress);
+                                    fullAddress = address+", "+ area +", "+ city + ", "+ country +", "+ postalCode;
 
                                 } catch (IOException e) {
                                     e.printStackTrace();
                                 }
-                                parkingSpotCoordinates.setText(latitude + ", " + longitude);
+
                             }
                         }
                     });
